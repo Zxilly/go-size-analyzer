fd99830cc39772c90dae52111e142b5856e61b68
Zxilly <zhouxinyu1001@gmail.com>
2024-04-23 18:19:05 +0800

    chore: split version print

--- cmd/gsa/main.go
+++ cmd/gsa/main.go
@@ -9,7 +9,6 @@ import (
 	"github.com/pkg/browser"
 	"log/slog"
 	"os"
-	"runtime/debug"
 )
 
 func main() {
@@ -25,21 +24,7 @@ func main() {
 	}
 
 	if options.Version {
-		fmt.Println("Version:", Version)
-		info, ok := debug.ReadBuildInfo()
-		if !ok {
-			fmt.Println("Failed to read build info")
-			os.Exit(1)
-		}
-		for _, m := range info.Settings {
-			switch m.Key {
-			case "vcs.revision":
-				fmt.Printf("Git revision: %s\n", m.Value)
-			case "vcs.time":
-				fmt.Printf("Build time: %s\n", m.Value)
-			}
-		}
-		os.Exit(0)
+		PrintVersionAndExit()
 	}
 
 	result, err := internal.Analyze(options.Arg.Binary)
--- cmd/gsa/version.go
+++ cmd/gsa/version.go
@@ -1,3 +1,27 @@
 package main
 
-const Version = "v0.2.0"
+import (
+	"fmt"
+	"os"
+	"runtime/debug"
+)
+
+const Version = "0.2.1"
+
+func PrintVersionAndExit() {
+	fmt.Println("Version:", Version)
+	info, ok := debug.ReadBuildInfo()
+	if !ok {
+		fmt.Println("Failed to read build info")
+		os.Exit(1)
+	}
+	for _, m := range info.Settings {
+		switch m.Key {
+		case "vcs.revision":
+			fmt.Printf("Git revision: %s\n", m.Value)
+		case "vcs.time":
+			fmt.Printf("Build time: %s\n", m.Value)
+		}
+	}
+	os.Exit(0)
+}
