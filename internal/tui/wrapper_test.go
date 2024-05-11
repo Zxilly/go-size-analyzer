679a1c1f7e619db7841bbd61966e8626f8a24f58
Zxilly <zhouxinyu1001@gmail.com>
2024-05-11 20:33:11 +0800

    test: test unreachable branch
    make CI happy :(

---
+++ internal/tui/wrapper_test.go
@@ -0,0 +1,41 @@
+package tui
+
+import (
+	"github.com/stretchr/testify/assert"
+	"sync"
+	"testing"
+)
+
+func Test_newWrapper(t *testing.T) {
+	assert.Panics(t, func() {
+		newWrapper(nil)
+	})
+}
+
+func Test_wrapper_Description(t *testing.T) {
+	w := wrapper{}
+	assert.Panics(t, func() {
+		w.Description()
+	})
+}
+
+func Test_wrapper_Title(t *testing.T) {
+	w := wrapper{}
+	assert.Panics(t, func() {
+		w.Title()
+	})
+}
+
+func Test_wrapper_children(t *testing.T) {
+	w := wrapper{cacheOnce: &sync.Once{}}
+	assert.Panics(t, func() {
+		w.children()
+	})
+}
+
+func Test_wrapper_size(t *testing.T) {
+	w := wrapper{}
+	assert.Panics(t, func() {
+		w.size()
+	})
+}
