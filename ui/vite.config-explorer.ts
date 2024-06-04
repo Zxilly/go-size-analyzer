0b8f52e8ef2763ecbda6c251d608d7d756b62e5c
Zxilly <zxilly@outlook.com>
2024-06-04 14:07:32 +0800

    feat: enable telemetry

--- ui/vite.config-explorer.ts
+++ ui/vite.config-explorer.ts
@@ -2,17 +2,25 @@ import {defineConfig} from 'vite';
 import {build, codecov, commonPlugin, getVersionTag} from "./common";
 import {createHtmlPlugin} from "vite-plugin-html";
 
+const tags = [getVersionTag()];
+if (process.env.GSA_TELEMETRY) {
+    tags.push({
+        tag: "script",
+        attrs: {
+            defer: true,
+            src: "https://trail.learningman.top/script.js",
+            "data-website-id": "1aab8912-b4b0-4561-a683-81a730bdb944",
+        }
+    })
+}
+
 export default defineConfig({
     plugins: [
         ...commonPlugin(),
         createHtmlPlugin({
             minify: true,
             entry: './src/explorer_main.tsx',
-            inject: {
-                tags: [
-                    getVersionTag(),
-                ]
-            }
+            inject: {tags}
         }),
         codecov("gsa-explorer")
     ],
