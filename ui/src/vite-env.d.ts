2753f97acf21f9b128e88ed8775258dea0399958
Zxilly <zxilly@outlook.com>
2024-06-07 16:06:51 +0800

    chore: remove global var

--- ui/src/vite-env.d.ts
+++ ui/src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-    readonly PACKAGE_DATA: string
-}
-
-interface ImportMeta {
-    readonly env: ImportMetaEnv
-}
-
-declare const PACKAGE_DATA: string;
