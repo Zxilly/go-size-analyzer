c1a398993599cc06aea55eb6c3caaf9a327a466a
Zxilly <zxilly@outlook.com>
2024-07-04 15:40:02 +0800

    style: rename IsJson to IsJSON
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- internal/diff/load.go
+++ internal/diff/load.go
@@ -15,7 +15,7 @@ func autoLoadFile(name string, options internal.Options) (*commonResult, error) 
 	}
 
 	r := new(commonResult)
-	if utils.IsJson(reader) {
+	if utils.IsJSON(reader) {
 		err = json.UnmarshalRead(utils.NewReaderAtAdapter(reader), r)
 		if err != nil {
 			return nil, err
