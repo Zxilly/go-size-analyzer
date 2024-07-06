cc5490fa13f4eefb03cb1ebc769fd32103461f45
Zxilly <zxilly@outlook.com>
2024-07-06 15:03:30 +0800

    test: add analyze warn test
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- internal/diff/load_test.go
+++ internal/diff/load_test.go
@@ -2,10 +2,14 @@ package diff
 
 import (
 	"io"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/Zxilly/go-size-analyzer/internal/entity"
+	"github.com/Zxilly/go-size-analyzer/internal/printer"
 	"github.com/Zxilly/go-size-analyzer/internal/test"
 )
 
@@ -43,5 +47,29 @@ func TestDiffJSONAndBinary(t *testing.T) {
 }
 
 func TestDifferentAnalyzer(t *testing.T) {
+	dir := t.TempDir()
+	first := filepath.Join(dir, "first")
+	second := filepath.Join(dir, "second")
 
+	createFile := func(name string, analyzers []entity.Analyzer) {
+		f, err := os.OpenFile(name, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0o644)
+		require.NoError(t, err)
+		defer func() {
+			require.NoError(t, f.Close())
+		}()
+
+		r := commonResult{
+			Analyzers: analyzers,
+		}
+
+		require.NoError(t, printer.JSON(r, f, &printer.JSONOption{}))
+	}
+
+	createFile(first, []entity.Analyzer{entity.AnalyzerDwarf, entity.AddrSourceSymbol})
+	createFile(second, []entity.Analyzer{entity.AnalyzerDisasm})
+
+	require.Error(t, Diff(io.Discard, DOptions{
+		OldTarget: first,
+		NewTarget: second,
+	}))
 }
