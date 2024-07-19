cd10c5eb272b200de1faac3b51ee7a59985d8bc1
Zxilly <zxilly@outlook.com>
2024-07-19 20:14:37 +0800

    build: write multi mem profile
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- cmd/gsa/profiler_main.go
+++ cmd/gsa/profiler_main.go
@@ -3,7 +3,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"path/filepath"
+	"runtime/pprof"
+	"time"
 
 	"github.com/knadh/profiler"
 
@@ -18,13 +23,12 @@ func main() {
 		panic("OUTPUT_DIR environment variable is not set")
 	}
 
-	targets := []int{profiler.Cpu, profiler.Mem, profiler.Mutex, profiler.Goroutine, profiler.Block, profiler.ThreadCreate, profiler.Trace}
+	targets := []int{profiler.Cpu, profiler.Mutex, profiler.Goroutine, profiler.Block, profiler.ThreadCreate, profiler.Trace}
 
 	p := profiler.New(
 		profiler.Conf{
 			DirPath:        outputDir,
 			NoShutdownHook: true,
-			MemProfileType: "heap",
 		},
 		targets...,
 	)
@@ -32,7 +36,61 @@ func main() {
 	p.Start()
 	defer p.Stop()
 
+	startWriteHeapProfile(outputDir)
+	defer stopWriteHeapProfile()
+
+	pprof.Lookup("heap")
+
 	if err := entry(); err != nil {
 		utils.FatalError(err)
 	}
+}
+
+var heapProfileStop context.CancelFunc
+
+func startWriteHeapProfile(outputDir string) {
+
+	var ctx context.Context
+	ctx, heapProfileStop = context.WithCancel(context.Background())
+
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		defer ticker.Stop()
+
+		id := 0
+
+		write := func() {
+			id++
+			path := filepath.Join(outputDir, fmt.Sprintf("mem-%d.pprof", id))
+			f, err := os.Create(path)
+			defer func(f *os.File) {
+				err = f.Close()
+				if err != nil {
+					panic(err)
+				}
+			}(f)
+			if err != nil {
+				panic(err)
+			}
+
+			err = pprof.WriteHeapProfile(f)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		for {
+			select {
+			case <-ctx.Done():
+				write()
+				return
+			case <-ticker.C:
+				write()
+			}
+		}
+	}()
+}
+
+func stopWriteHeapProfile() {
+	heapProfileStop()
 }
