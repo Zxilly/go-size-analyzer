b0803c1c247d8bb37b053e64025b8c150bb9e0d9
Zxilly <zxilly@outlook.com>
2024-10-21 05:23:01 +0800

    test: update macho test
    Signed-off-by: Zxilly <zxilly@outlook.com>

--- internal/wrapper/macho_test.go
+++ internal/wrapper/macho_test.go
@@ -1,27 +1,29 @@
 package wrapper
 
 import (
-	"debug/macho"
 	"testing"
+
+	"github.com/blacktop/go-macho"
+	"github.com/blacktop/go-macho/types"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGoArchReturnsCorrectArchitectureString(t *testing.T) {
 	tests := []struct {
-		cpu      macho.Cpu
+		cpu      types.CPU
 		expected string
 	}{
-		{macho.Cpu386, "386"},
-		{macho.CpuAmd64, "amd64"},
-		{macho.CpuArm, "arm"},
-		{macho.CpuArm64, "arm64"},
-		{macho.CpuPpc64, "ppc64"},
-		{macho.Cpu(0), ""}, // Unsupported CPU type
+		{types.CPUI386, "386"},
+		{types.CPUAmd64, "amd64"},
+		{types.CPUArm, "arm"},
+		{types.CPUArm64, "arm64"},
+		{types.CPUPpc64, "ppc64"},
+		{types.CPU(0), ""}, // Unsupported CPU type
 	}
 
 	for _, test := range tests {
-		m := MachoWrapper{file: &macho.File{FileHeader: macho.FileHeader{Cpu: test.cpu}}}
+		m := MachoWrapper{file: &macho.File{FileTOC: macho.FileTOC{FileHeader: types.FileHeader{CPU: test.cpu}}}}
 		result := m.GoArch()
 		assert.Equal(t, test.expected, result)
 	}
