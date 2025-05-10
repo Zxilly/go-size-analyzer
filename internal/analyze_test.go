f5e4c8ab96109e45d37eaeda401e48392d4aa988
Zxilly <zxilly@outlook.com>
2025-05-10 17:47:37 +0800

    test: add test for imports

--- internal/analyze_test.go
+++ internal/analyze_test.go
@@ -2,9 +2,11 @@ package internal
 
 import (
 	"bytes"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"golang.org/x/exp/mmap"
 )
 
 func FuzzAnalyze(f *testing.F) {
@@ -17,4 +19,39 @@ func FuzzAnalyze(f *testing.F) {
 			}
 		})
 	})
+}
+
+func GetCurrentRunningBinary(t *testing.T) string {
+	t.Helper()
+
+	path, err := os.Executable()
+	require.NoError(t, err)
+
+	return path
+}
+
+func TestAnalyzeImports(t *testing.T) {
+	bin := GetCurrentRunningBinary(t)
+
+	f, err := mmap.Open(bin)
+	require.NoError(t, err)
+	defer func() {
+		err := f.Close()
+		require.NoError(t, err)
+	}()
+
+	result, err := Analyze(bin, f, uint64(f.Len()), Options{
+		SkipDisasm: true,
+		SkipDwarf:  true,
+		SkipSymbol: true,
+		Imports:    true,
+	})
+	require.NoError(t, err)
+
+	require.NotNil(t, result)
+
+	testingPkg := result.Packages["testing"]
+	require.NotNil(t, testingPkg)
+
+	require.Contains(t, testingPkg.ImportedBy, "github.com/Zxilly/go-size-analyzer/internal")
 }
