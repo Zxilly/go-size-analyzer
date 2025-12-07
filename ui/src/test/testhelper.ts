eadeca14d1dcc8093c9351f14d4279405f763f2b
Zxilly <zxilly@outlook.com>
2025-12-07 19:58:20 +0800

    refactor: update import statements to use node: prefix

--- ui/src/test/testhelper.ts
+++ ui/src/test/testhelper.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from "fs";
-import { join, dirname } from "path";
-import { fileURLToPath } from "url";
+import { readFileSync } from "node:fs";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 import { assert } from "vitest";
 import { parseResult } from "../schema/schema.ts";
 
